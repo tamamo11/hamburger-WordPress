5c19fafa53f038348a1df7fdc10f9857b477f256
tamamo11 <masa11mobile@gmail.com>
2023-10-05 14:30:31 +0900

    検索機能修正、個別固定ページレイアウト修正

--- functions.php
+++ functions.php
@@ -8,22 +8,24 @@
             'gallery',
             'caption',
         ));
+        add_theme_support('post-thumbnails');
+        add_theme_support( 'menus' );
+        add_theme_support( 'title-tag' );
+        add_theme_support( 'custom-spacing' );
+        register_nav_menus(array(
+            'footer_nav'=> esc_html__('footer navigation', 'footer navigation'),
+            'category_nav'=> esc_html__('category navigation', 'category navigation'),
+            'hamburger_nav'=> esc_html__('hamburger', 'hamburger'),
+            'sidemenu_nav'=> esc_html__('sidemenu', 'sidemenu'),
+            'drink_nav'=> esc_html__('drink', 'drink'),
+    
+        ));
+        add_theme_support('editor-styles');
+        add_theme_support( 'align-wide' );
+        add_theme_support( 'custom-line-height' );
+        add_theme_support( 'appearance-tools' );
     }
     //テーマサポート
-    add_theme_support('post-thumbnails');
-    add_theme_support( 'menus' );
-    add_theme_support( 'title-tag' );
-    add_theme_support( 'custom-spacing' );
-    register_nav_menus(array(
-        'footer_nav'=> esc_html__('footer navigation', 'footer navigation'),
-        'category_nav'=> esc_html__('category navigation', 'category navigation'),
-        'hamburger_nav'=> esc_html__('hamburger', 'hamburger'),
-        'sidemenu_nav'=> esc_html__('sidemenu', 'sidemenu'),
-        'drink_nav'=> esc_html__('drink', 'drink'),
-
-    ));
-    add_theme_support('editor-styles');
-    add_editor_style('editor-styles.css');
 
     add_action('after_setup_theme', 'custom_theme_support');
 
